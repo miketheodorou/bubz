d21c3ef699abb5a262184b704f841a82ad438589
Mike Theodorou <miketheocodes@gmail.com>
2025-06-13 23:07:40 -0600

    chore: removes default pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -31,9 +30,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name='index'
         options={{
-          title: 'Home',
+          title: 'Food',
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='house.fill' color={color} />
+            <MaterialCommunityIcons size={28} name='bowl' color={color} />
           )
         }}
       />
@@ -47,15 +46,6 @@ export default function TabLayout() {
               name='dog-service'
               color={color}
             />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name='explore'
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='paperplane.fill' color={color} />
           )
         }}
       />
