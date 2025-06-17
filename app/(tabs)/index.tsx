11fbe7e845114ebd42e8069852294c51b79827b3
Mike Theodorou <miketheocodes@gmail.com>
2025-06-16 21:58:06 -0600

    feat: adds basic page styling and time flip based on time of date

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,72 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, TouchableOpacity, View } from 'react-native';
 
 export default function FoodView() {
+  const [mealType, setMealType] = useState<'Breakfast' | 'Dinner'>('Breakfast');
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    // Function to determine if it's breakfast or dinner time
+    const determineMealTime = () => {
+      const now = new Date();
+      const hour = now.getHours();
+
+      // If time is between 12AM and 3PM (15:00), it's breakfast time
+      // Otherwise, it's dinner time
+      if (hour >= 0 && hour < 15) {
+        setMealType('Breakfast');
+      } else {
+        setMealType('Dinner');
+      }
+    };
+
+    determineMealTime();
+
+    // Update meal type every minute
+    const interval = setInterval(determineMealTime, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <ThemedView
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-    >
-      <ThemedText>Did Lily Eat?</ThemedText>
+    <ThemedView style={{ flex: 1, paddingHorizontal: 16 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ paddingTop: 32 }}>
+          <ThemedText
+            style={{ fontSize: 48, fontWeight: '900', lineHeight: 56 }}
+          >
+            {mealType} Time
+          </ThemedText>
+          <ThemedText style={{ fontWeight: '500' }}>
+            Has Lily eaten {mealType.toLocaleLowerCase()} yet?
+          </ThemedText>
+        </View>
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <TouchableOpacity
+            style={{
+              padding: 16,
+              backgroundColor: '#a855f7',
+              borderRadius: '100%',
+              aspectRatio: 1 / 1,
+              width: '50%',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <FontAwesome6
+              name='bowl-rice'
+              size={56}
+              color={Colors[colorScheme ?? 'light']?.tint}
+            />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </ThemedView>
   );
 }
